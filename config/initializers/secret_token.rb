5a8f0d8c08680da886371d1a7e0c7b8fd60f3338
user <user@test.ru>
2012-10-08 19:28:06 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorTest::Application.config.secret_token = 'e5ee1ed873a6f485327f0540889cb359db79def4fcae92c7d4d803d1761462db755e891251bf527b0ee8724f340f6000c664b0b699b642e7122c3690a7d26a4e'
